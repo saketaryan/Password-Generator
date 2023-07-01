a5bbb4c51c9f987e832332d5eb3b358122fecee6
Saket Aryan <135685386+saketaryan@users.noreply.github.com>
2023-07-02 00:52:47 +0530

    added password generator prompts checks and generate password

--- src/components/Strength.jsx
+++ src/components/Strength.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const Strength = () => {
+const Strength = (props) => {
   return (
     <div className='flex strength'>
         <div className='w-100'>
         <div className='flex justify-between box bg-dark'>
             <h4 className='my-auto strength'>STRENGTH</h4>
             <div className='flex justify-between'>
-                <div className={`box-st`}></div>
-                <div className={`box-st`}></div>
-                <div className={`box-st`}></div>
-                <div className={`box-st`}></div>
+                <div className={`box-st ${props.strength>=1&&'selected'}`}></div>
+                <div className={`box-st ${props.strength>=2&&'selected'}`}></div>
+                <div className={`box-st ${props.strength>=3&&'selected'}`}></div>
+                <div className={`box-st ${props.strength>=4&&'selected'}`}></div>
             </div>
         </div>
         </div>
